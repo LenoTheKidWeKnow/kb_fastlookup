496ea270329ee1ff6924b8d3c05211bd0bda9da8
Lenorandum <46946734+LenoTheKidWeKnow@users.noreply.github.com>
2022-03-25 10:40:08 +0900

    Update uploadPyPi.sh

--- uploadPyPi.sh
+++ uploadPyPi.sh
@@ -1,6 +1,6 @@
 rm -rf build
 rm -rf dist
-rm -rf simple_bank_korea.egg-info
+rm -rf kb_fastlookup.egg-info
 python setup.py sdist
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 twine upload dist/*
